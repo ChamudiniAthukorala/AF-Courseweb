d402985f04cbd4ce9c543b9db979e562a8e189eb
HirushaKumarasinghe <hirushakumarasinghe@gmail.com>
2019-05-03 09:34:56 +0530

    third commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 //load UserModel
 const User = require("../../models/UserModels");
@@ -17,11 +19,29 @@ router.post("/register", (req, res) => {
     if (user) {
       return res.status(400).json({ email: "Email already exists" });
     } else {
+      const avatar = gravatar.url(req.body.email, {
+        //this varible is used forupload profile sign image to database
+        s: "200", //size
+        r: "pg", //rating
+        d: "mm" //default
+      });
+
       const newUser = new User({
         name: req.body.name,
         email: req.body.email,
-        avatar: avatar,
+        avatar,
         password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
       });
     }
   });
