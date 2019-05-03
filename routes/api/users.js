f035de9b42769d0a77bd0b3df82c201f94a7d1e9
HirushaKumarasinghe <hirushakumarasinghe@gmail.com>
2019-05-03 08:50:17 +0530

    second commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
+//load UserModel
+const User = require("../../models/UserModels");
+
 // @route   GET api/users/test
 // @desc    Test users route
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "users works" }));
 
+// @route   GET api/users/register
+// @desc    Register users
+// @access  Public
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar: avatar,
+        password: req.body.password
+      });
+    }
+  });
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const users = require("./routes/api/users");
 const studets = require("./routes/api/student");
@@ -8,8 +9,13 @@ const notifications = require("./routes/api/notification");
 
 const app = express();
 
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 //DB Config
 const db = require("./config/keys").mongoURI;
+
 //connect to mongoose
 mongoose
   .connect(db)
