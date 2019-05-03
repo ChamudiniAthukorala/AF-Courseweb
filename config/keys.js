2a3222901bb37a3baff832fb20da1e0fca0685c5
HirushaKumarasinghe <hirushakumarasinghe@gmail.com>
2019-05-03 09:37:27 +0530

    third commit

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://coursewebadmin:abch@cluster0-ti88x.mongodb.net/test?retryWrites=true"
+    "mongodb+srv://coursewebadmin:abch@cluster0-ti88x.mongodb.net/test?retryWrites=true",
+  useNewUrlParser: true
 };
