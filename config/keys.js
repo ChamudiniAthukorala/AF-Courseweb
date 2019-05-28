3114129673fa5d4e7ea063016ca65203404f9f28
ChamudiniAthukorala <chamudiniathu@gmail.com>
2019-05-28 12:21:58 +0530

    fourth commit

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,8 @@
 module.exports = {
-  mongoURI:
-    "mongodb+srv://coursewebadmin:abch@cluster0-ti88x.mongodb.net/test?retryWrites=true",
-  useNewUrlParser: true
+  mongoURI: "mongodb://localhost/courseweb",
+  secretOrKey: "secret"
 };
+
+//  mongoURI: "mongodb://localhost/courseweb"
+//  mongoURI: "mongodb+srv://coursewebadmin:abch@cluster0-ti88x.mongodb.net/test?retryWrites=true"
+//  2nd one is for hosting
---
+++ validation/register.js
@@ -0,0 +1,44 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateRegisterInpu(data) {
+  let errors = {};
+
+  data.name = !isEmpty(data.name) ? data.name : "";
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.confirmPassword = !isEmpty(data.confirmPassword)
+    ? data.confirmPassword
+    : "";
+
+  if (!Validator.isLength(data.name, { min: 8 })) {
+    errors.name = "Name must be more than 8 characters";
+  }
+
+  if (Validator.isEmpty(data.name)) {
+    errors.name = "Name field is required";
+  }
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  }
+  //email validation
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+  if (Validator.isEmpty(data.confirmPassword)) {
+    errors.confirmPassword = "Password confirmation field is required";
+  }
+
+  //password match
+  if (!Validator.equals(data.password, data.confirmPassword)) {
+    errors.confirmPassword = "Password must match";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
